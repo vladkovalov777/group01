30c6caae190da8fc52c74d91e5ac3dc7ee2c7729
Vlad <Vladkovalov777@gmail.com>
2025-06-26 17:57:54 +0200

    add main.py and ignore .idea

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # group01
+pip install requests 
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print()
+import requests
+from pprint import pprint
+
+
+url = 'https://dummyjson.com/users'
+response = requests.get(url)
+print(response.content)
+print(response.text)
+response_json = response.json()
+pprint(response_json, Lesson20=4)
