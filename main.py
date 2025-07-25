eddd011807985b55f43b6c2f142650689257b944
Vlad <Vladkovalov777@gmail.com>
2025-07-25 17:02:46 +0200

    Add script for summing user order costs

--- main.py
+++ main.py
@@ -10,26 +10,31 @@ client = MongoClient(uri, server_api=ServerApi("1"))
 database = client["books_database"]
 books_collection = database["books"]
 
-got_book = {
-    "назва": "Гра престолів",
-    "вартість": 350,
-    "рік_випуску": 1996,
-    "кількість_сторінок": 694,
-}
-books_collection.insert_one(got_book)
+print("\n✅ Усі книги:")
+for book in books_collection.find():
+    print(book)
 
-school_books = [
-    {"назва": "Математика 9 клас", "клас": 9, "кількість_сторінок": 230},
-    {"назва": "Фізика для школярів", "клас": 9, "кількість_сторінок": 210},
-    {"назва": "Історія України. Частина 1", "клас": 9, "кількість_сторінок": 255},
-    {"назва": "Англійська мова. Рівень B1", "клас": 9, "кількість_сторінок": 185},
-    {"назва": "Основи здоров’я", "клас": 9, "кількість_сторінок": 170},
-]
-books_collection.insert_many(school_books)
+print("\n✅ Книга з назвою 'Гра престолів':")
+book = books_collection.find_one({"назва": "Гра престолів"})
+print(book)
 
+print("\n✅ Книги з кількістю сторінок більше 200:")
+for book in books_collection.find({"кількість_сторінок": {"$gt": 200}}):
+    print(book)
 
-try:
-    client.admin.command("ping")
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+new_price = 450
+result = books_collection.update_one(
+    {"назва": "Гра престолів"}, {"$set": {"вартість": new_price}}
+)
+print(f"\n✅ Оновлено {result.modified_count} книгу(и) — нова ціна: {new_price}")
+
+result = books_collection.update_many({"клас": 9}, {"$set": {"favorite": True}})
+print(f"\n✅ Додано поле 'favorite' до {result.modified_count} книги(и)")
+
+delete_result = books_collection.delete_one({"назва": "Основи здоров’я"})
+print(f"\n✅ Видалено книгу 'Основи здоров’я' — {delete_result.deleted_count} документ")
+
+delete_many_result = books_collection.delete_many({"кількість_сторінок": {"$lt": 100}})
+print(
+    f"\n✅ Видалено всі книги з <100 сторінок — {delete_many_result.deleted_count} документ(ів)"
+)
