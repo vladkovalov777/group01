1d4194856db3ad54ceb956021ab3493834d30aee
Vlad <Vladkovalov777@gmail.com>
2025-07-25 17:16:50 +0200

    Add script for summing user order costs

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ client = MongoClient(uri, server_api=ServerApi("1"))
 database = client["books_database"]
 books_collection = database["books"]
 
-print("\n✅ Усі книги:")
+print("\n Усі книги:")
 for book in books_collection.find():
     print(book)
 
-print("\n✅ Книга з назвою 'Гра престолів':")
+print("\n Книга з назвою 'Гра престолів':")
 book = books_collection.find_one({"назва": "Гра престолів"})
 print(book)
 
-print("\n✅ Книги з кількістю сторінок більше 200:")
+print("\n Книги з кількістю сторінок більше 200:")
 for book in books_collection.find({"кількість_сторінок": {"$gt": 200}}):
     print(book)
 
@@ -26,15 +26,15 @@ new_price = 450
 result = books_collection.update_one(
     {"назва": "Гра престолів"}, {"$set": {"вартість": new_price}}
 )
-print(f"\n✅ Оновлено {result.modified_count} книгу(и) — нова ціна: {new_price}")
+print(f"\n Оновлено {result.modified_count} книгу(и) — нова ціна: {new_price}")
 
 result = books_collection.update_many({"клас": 9}, {"$set": {"favorite": True}})
-print(f"\n✅ Додано поле 'favorite' до {result.modified_count} книги(и)")
+print(f"\n Додано поле 'favorite' до {result.modified_count} книги(и)")
 
 delete_result = books_collection.delete_one({"назва": "Основи здоров’я"})
-print(f"\n✅ Видалено книгу 'Основи здоров’я' — {delete_result.deleted_count} документ")
+print(f"\n Видалено книгу 'Основи здоров’я' — {delete_result.deleted_count} документ")
 
 delete_many_result = books_collection.delete_many({"кількість_сторінок": {"$lt": 100}})
 print(
-    f"\n✅ Видалено всі книги з <100 сторінок — {delete_many_result.deleted_count} документ(ів)"
+    f"\n Видалено всі книги з <100 сторінок — {delete_many_result.deleted_count} документ(ів)"
 )
